a5cc7637703f9f97e95cc355d1c6957415dd0db4
Konnov <strouk7@yandex.ru>
2024-01-20 17:48:58 +0300

    add update phone

--- src/main/java/com/example/PhoneShop/controllers/PhoneController.java
+++ src/main/java/com/example/PhoneShop/controllers/PhoneController.java
@@ -1,5 +1,6 @@
 package com.example.PhoneShop.controllers;
 
+import com.example.PhoneShop.models.Company;
 import com.example.PhoneShop.models.Phone;
 import com.example.PhoneShop.services.CompanyService;
 import com.example.PhoneShop.services.PhoneService;
@@ -11,6 +12,11 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * Контроллер для класса {@link Phone}.
@@ -98,5 +104,56 @@ public class PhoneController {
 
         return "redirect:/phone";
     }
+
+    @GetMapping("/update/{id}")
+    public String updateGet(@RequestParam(value = "nameExist", required = false) String nameExist,
+                            @ModelAttribute("phone") Phone phone, @PathVariable("id") Long id,
+                            Model model) {
+
+        if (phone.getName() == null) {
+            phone = phoneService.getPhoneById(id);
+            if (phone == null) {
+                log.error("There is no phone with id=" + id);
+                return "error/404";
+            }
+
+        }
+
+        Iterable<Company> it = companyService.getAllCompanies();
+        List<Company> companies = new ArrayList<>();
+
+        it.forEach(companies::add);
+        companies.remove(phone.getCompany());
+
+
+        model.addAttribute("phone", phone);
+        model.addAttribute("companies", companies);
+        model.addAttribute("nameExist", nameExist != null);
+
+        return "phone/update";
+    }
+
+    @PostMapping("/update/{id}")
+    public String updatePost(@ModelAttribute("phone") @Valid Phone phone, BindingResult bindingResult,
+                             @PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult.hasErrors(). Phone to redirect = " + phone);
+            return "phone/update";
+        }
+
+        Phone phoneByName = phoneService.getPhoneByName(phone.getName());
+
+        if (phoneByName != null && !Objects.equals(phoneByName.getId(), phone.getId())) {
+            redirectAttributes.addFlashAttribute("phone", phone);
+            log.info("Phone with this name exist. Phone to redirect = " + phone);
+            return "redirect:/phone/update/" + id + "?nameExist";
+        }
+
+        phoneService.savePhone(phone);
+
+        return "redirect:/phone/" + id;
+    }
+
 
 }
